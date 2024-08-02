076951b26a3034fa915fa968d3abcaf789b52519
Lauren Smith <mealerlauren@gmail.com>
2024-08-02 13:25:41 -0500

    signup functionality

--- src/app/signup/SignupForm.tsx
+++ src/app/signup/SignupForm.tsx
@@ -1,45 +1,72 @@
 // src/app/signup/SignupForm.tsx
-import React from "react";
-import './signup.css'
+"use client";
+
+import React, { useState } from "react";
+import "./signup.css";
 
 const SignupForm: React.FC = () => {
-    return (
-        <div className='form'>
-            <div className='title'>Welcome</div>
-            <div className='subtitle'>Let's create your account!</div>
-            <div className='input-container ic1'>
-                <input id='name' className='name-input' type='text' placeholder='Name' required/>
-                {/* <div className='cut'></div> */}
-                {/* <label htmlFor='name' className='placeholder'>Name</label> */}
-            </div>
-            <div className='input-container ic2'>
-                <input id='password' className='password-input' type='text' placeholder='Password' required/>
-                {/* <div className='cut'></div> */}
-                {/* <label htmlFor='password' className='placeholder'>Password</label> */}
-            </div>
-            <div className='input-container ic3'>
-                <input id='email' className='email-input' type='email' placeholder='Email' required/>
-                {/* <div className='cut cut-short'></div> */}
-                {/* <label htmlFor='email' className='placeholder'>Email</label> */}
-            </div>
-            <button type='text' class='submit'>Submit</button>
-        </div>
-        // <form>
-        //     <label>
-        //         Name:
-        //         <input type='name' name='name' required />
-        //     </label>
-        //     <label>
-        //         Email:
-        //         <input type='email' name='email' required />
-        //     </label>
-        //     <label>
-        //         Password:
-        //         <input type='password' name='password' required />
-        //     </label>
-        //     <button type='submit'>Signup</button>
-        // </form>
-    );
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    console.log("Form submitted with:", {
+      name,
+      email,
+      password,
+    });
+
+    console.log("Signup successful");
+
+    setName('');
+    setPassword('');
+    setEmail('');
+  };
+
+  return (
+    <form className="form" onSubmit={handleSubmit}>
+      <div className="title">Welcome</div>
+      <div className="subtitle">Let's create your account!</div>
+      <div className="input-container ic1">
+        <input
+          id="name"
+          className="name-input"
+          type="text"
+          placeholder="Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          required
+        />
+      </div>
+      <div className="input-container ic2">
+        <input
+          id="password"
+          className="password-input"
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+      </div>
+      <div className="input-container ic3">
+        <input
+          id="email"
+          className="email-input"
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+      </div>
+      <button type="submit" className="submit">
+        Submit
+      </button>
+    </form>
+  );
 };
 
 export default SignupForm;
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -5,8 +5,21 @@ import SignupForm from './SignupForm';
 const SignupPage: React.FC = () => {
   return (
     <div>
-      <h1>Signup</h1>
-      <SignupForm />
+      <div className='topnav'>
+        <nav>
+          <ul>
+            <li>
+              <a className='topnav-btn' href='/'>Home</a>
+              <a className='topnav-btn' href='/login'>Login</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+
+      <div>
+        <h1></h1>
+        <SignupForm />
+      </div>
     </div>
   );
 };
